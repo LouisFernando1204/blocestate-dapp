fa4e588ce073cd32cdc007df07143fdab4e4af00
Your Name <samuelmiracle.lie@gmail.com>
2024-10-30 22:58:58 +0900

    AuctionDetail Countdown; further progress

--- src/views/Auction.jsx
+++ src/views/Auction.jsx
@@ -15,7 +15,7 @@ const Auction = () => {
     propertyAuctionCards: [
       { address: "Bikini Bottom No. 42, Los Angeles", propertyType: "House", startingBid: 5, startBid_Date: "2024-05-05", endBit_Date: "2024-05-19", image: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/380dbd01-38ee-4146-93df-31ce051f4b83/dfodfos-503fd0f7-8823-4c7d-ab84-753ed00e5e93.png/v1/fill/w_847,h_943,q_70,strp/_sbsp__the_chum_bucket_by_spongedrew250_dfodfos-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzM4MGRiZDAxLTM4ZWUtNDE0Ni05M2RmLTMxY2UwNTFmNGI4M1wvZGZvZGZvcy01MDNmZDBmNy04ODIzLTRjN2QtYWI4NC03NTNlZDAwZTVlOTMucG5nIiwiaGVpZ2h0IjoiPD0yMTM2Iiwid2lkdGgiOiI8PTE5MjAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uud2F0ZXJtYXJrIl0sIndtayI6eyJwYXRoIjoiXC93bVwvMzgwZGJkMDEtMzhlZS00MTQ2LTkzZGYtMzFjZTA1MWY0YjgzXC9zcG9uZ2VkcmV3MjUwLTQucG5nIiwib3BhY2l0eSI6OTUsInByb3BvcnRpb25zIjowLjQ1LCJncmF2aXR5IjoiY2VudGVyIn19.qGf1PFS1m4MBgmH9wnHwcHWB0mKU9dKMLCZxEzkRw5c", area: 240 },
       { address: "International House II, 45 Jurye-ro", propertyType: "Student Dormitory", startingBid: 25, startBid_Date: "2024-05-8", endBit_Date: "2024-05-22", image: "https://i.namu.wiki/i/qW2jMif6OYol0suRznfNCRWwxxoPJkfz26RP2CFBytbiDxXfXzVljtawQnSUfSVAvbUjiRW6hKWvASvIR7QPRg.webp", area: 2000 },
-      { address: "Jl. HR Muhammad No. 25, Pradahkalikendal", propertyType: "Surga Dunia", startingBid: 200, startBid_Date: "2024-05-19", endBit_Date: "2024-06-01", image: "https://imgcdn.espos.id/@espos/images/2022/10/gacoan.jpg", area: 1250 },
+      { address: "Jl. HR Muhammad No. 25, Pradahkalikendal", propertyType: "Surga Dunia", startingBid: 200, startBid_Date: "2024-10-16", endBit_Date: "2024-10-30", image: "https://imgcdn.espos.id/@espos/images/2022/10/gacoan.jpg", area: 1250 },
       { address: "Jl. Gajahmada No.107, Miroto, Semarang", propertyType: "Restaurant", startingBid: 175, startBid_Date: "2024-05-8", endBit_Date: "2024-05-22", image: "https://indonesiatraveler.id/wp-content/uploads/2020/03/Lunpia-Cik-Me-Me3.jpg", area: 1089 },
     ],
   };
@@ -65,13 +65,16 @@ const Auction = () => {
                     class="rounded-lg border border-gray-200 bg-white p-6 shadow-xl hover:scale-105 duration-200 ">
 
                     <a href={`/auction/${index}`}>
-                      <div class="h-56 w-full">
+                      <div class="relative h-56 w-full">
+                        <div class="absolute top-0 left-0 z-10 flex items-center gap-4 p-2 text-sm text-white bg-darkBrown shadow-md rounded-br-xl">
+                          <p>{auction.propertyType}</p>
+                        </div>
                         <img class="mx-auto h-full " src={auction.image} alt="" />
                       </div>
 
                       <div class="pt-6">
                         <a class="text-lg font-semibold leading-tight text-gray-900 hover:underline ">{auction.address}</a>
-                        <p class="mt-1 text-sm font-medium text-gray-500 ">{auction.propertyType} | {auction.area} ㎡</p>
+                        <p class="mt-1 text-sm font-medium text-gray-500 ">{auction.area} ㎡</p>
 
                         <ul class="mt-2 flex items-center gap-2">
                           <li class="flex items-center gap-2">
@@ -91,10 +94,8 @@ const Auction = () => {
                             <p class="text-2xl font-extrabold leading-tight text-gray-900 ">{auction.startingBid} ICP</p>
                           </div>
 
-                          <button type="button" class="inline-flex items-center rounded-lg bg-darkBrown px-5 py-2.5 text-sm font-medium text-white hover:bg-amber-800 focus:outline-none focus:ring-4  focus:ring-primary-300">
-                            <svg class="-ms-2 me-2 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6" />
-                            </svg>
+                          <button type="button" class="inline-flex items-center rounded-lg bg-amber-900 px-5 py-2.5 gap-2 text-sm font-medium text-white hover:bg-amber-800 focus:outline-none focus:ring-4 focus:ring-primary-300">
+                            <img class="w-4" src="\images\bidding.png" alt="arrow--v1" />
                             Bid
                           </button>
                         </div>
--- src/views/AuctionDetail.jsx
+++ src/views/AuctionDetail.jsx
@@ -1,7 +1,11 @@
 const AuctionDetail = () => {
   const dummyData = {
     propertyImage: [
-      "https://imgcdn.espos.id/@espos/images/2022/10/gacoan.jpg"
+      "https://imgcdn.espos.id/@espos/images/2022/10/gacoan.jpg",
+      "https://ex-pose.net/wp-content/uploads/2024/01/IMG-20240120-WA0007.jpg",
+      "https://media.suara.com/pictures/1600x840/2024/05/01/68116-presiden-jokowi-makan-mi-gacoan-di-mataram.jpg",
+      "https://asrinews.com/wp-content/uploads/2024/02/IMG_20240222_114134-scaled.jpg",
+      "https://klikfakta.com/wp-content/uploads/2024/06/IMG-20240612-WA0002.jpg"
     ],
     address: "Jl. HR Muhammad No. 25, Pradahkalikendal",
     city: "Surabaya",
@@ -10,8 +14,9 @@ const AuctionDetail = () => {
     description: "PENGEN UDANG KEJU GACOAN YAOLO Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum ex officiis fugiat dolore, explicabo ut est nihil sit optio? Minus autem architecto ducimus aspernatur voluptates dignissimos incidunt recusandae maiores non.",
     area: 1250,
     yearBuilt: 2020,
-    startBid_date: "2024-05-19",
-    endBid_date: "2024-06-01",
+    startBid_date: "2024-10-16",
+    endBid_date: "2024-10-30",
+    endBid_hour: "23:59",
     startingBid: 200,
     propertyType: "Surga Dunia",
 
@@ -54,129 +59,172 @@ const AuctionDetail = () => {
     document.getElementById('mainImage').src = src;
   };
 
+  let dest = new Date(`${dummyData.endBid_date}T${dummyData.endBid_hour}:00`).getTime();
+  let x = setInterval(function () {
+    let now = new Date().getTime();
+    let diff = dest - now;
+
+    if (diff <= 0) {
+      clearInterval(x); // Stop the countdown
+      return; // Exit the function
+    }
+
+    let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    if (days < 10) {
+      days = `0${days}`;
+    }
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+
+    let countdownElements = document.getElementsByClassName("countdown-element");
+
+    for (let i = 0; i < countdownElements.length; i++) {
+      let className = countdownElements[i].classList[1]; // Get the second class name
+      switch (className) {
+        case "days":
+          countdownElements[i].innerHTML = days;
+          break;
+        case "hours":
+          countdownElements[i].innerHTML = hours;
+          break;
+        case "minutes":
+          countdownElements[i].innerHTML = minutes;
+          break;
+        case "seconds":
+          countdownElements[i].innerHTML = seconds;
+          break;
+        default:
+          break;
+      }
+    }
+  }, 1000);
+
   return (
-    <div class="bg-gray-100">
-      <div class="container mx-auto px-4 py-8">
-        <div class="flex flex-wrap -mx-4">
-          {/* <!-- Product Images --> */}
-          <div class="w-full md:w-1/2 px-4 mb-8">
-            <img src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwxfHxoZWFkcGhvbmV8ZW58MHwwfHx8MTcyMTMwMzY5MHww&ixlib=rb-4.0.3&q=80&w=1080" alt="Product"
-              class="w-full h-auto rounded-lg shadow-md mb-4" id="mainImage"></img>
-            <div class="flex gap-4 py-4 justify-center overflow-x-auto">
-              <img src="https://images.unsplash.com/photo-1505751171710-1f6d0ace5a85?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwxMnx8aGVhZHBob25lfGVufDB8MHx8fDE3MjEzMDM2OTB8MA&ixlib=rb-4.0.3&q=80&w=1080" alt="Thumbnail 1"
-                class="size-16 sm:size-20 object-cover rounded-md cursor-pointer opacity-60 hover:opacity-100 transition duration-300"
-                onclick="changeImage(this.src)"></img>
-              <img src="https://images.unsplash.com/photo-1484704849700-f032a568e944?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw0fHxoZWFkcGhvbmV8ZW58MHwwfHx8MTcyMTMwMzY5MHww&ixlib=rb-4.0.3&q=80&w=1080" alt="Thumbnail 2"
-                class="size-16 sm:size-20 object-cover rounded-md cursor-pointer opacity-60 hover:opacity-100 transition duration-300"
-                onclick="changeImage(this.src)"></img>
-              <img src="https://images.unsplash.com/photo-1496957961599-e35b69ef5d7c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw4fHxoZWFkcGhvbmV8ZW58MHwwfHx8MTcyMTMwMzY5MHww&ixlib=rb-4.0.3&q=80&w=1080" alt="Thumbnail 3"
-                class="size-16 sm:size-20 object-cover rounded-md cursor-pointer opacity-60 hover:opacity-100 transition duration-300"
-                onclick="changeImage(this.src)"></img>
-              <img src="https://images.unsplash.com/photo-1528148343865-51218c4a13e6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwzfHxoZWFkcGhvbmV8ZW58MHwwfHx8MTcyMTMwMzY5MHww&ixlib=rb-4.0.3&q=80&w=1080" alt="Thumbnail 4"
-                class="size-16 sm:size-20 object-cover rounded-md cursor-pointer opacity-60 hover:opacity-100 transition duration-300"
-                onclick="changeImage(this.src)"></img>
-            </div>
+    <div class="mt-32 container mx-auto md:px-4 py-8">
+      <div class="flex flex-wrap -mx-4">
+        {/* <!-- Product Images --> */}
+        <div class="relative w-full md:w-1/2 px-4 mb-8">
+          <div class="absolute top-0 left-0 z-10 flex items-center gap-4 p-4 text-white bg-darkBrown shadow-md rounded-br-xl">
+            <p>{dummyData.propertyType}</p>
           </div>
-
-          {/* <!-- Product Details --> */}
-          <div class="w-full md:w-1/2 px-4">
-            <h2 class="text-3xl font-bold mb-2">{ dummyData.address }</h2>
-            <p class="text-gray-600 mb-4">{ dummyData.city }</p>
-            <div class="mb-4">
-              <span class="text-2xl font-bold mr-2">$349.99</span>
-              <span class="text-gray-500 line-through">$399.99</span>
-            </div>
-            <div class="flex items-center mb-4">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                class="size-6 text-yellow-500">
-                <path fill-rule="evenodd"
-                  d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-                  clip-rule="evenodd" />
-              </svg>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                class="size-6 text-yellow-500">
-                <path fill-rule="evenodd"
-                  d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-                  clip-rule="evenodd" />
-              </svg>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                class="size-6 text-yellow-500">
-                <path fill-rule="evenodd"
-                  d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-                  clip-rule="evenodd" />
-              </svg>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                class="size-6 text-yellow-500">
-                <path fill-rule="evenodd"
-                  d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-                  clip-rule="evenodd" />
-              </svg>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                class="size-6 text-yellow-500">
-                <path fill-rule="evenodd"
-                  d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-                  clip-rule="evenodd" />
-              </svg>
-              <span class="ml-2 text-gray-600">4.5 (120 reviews)</span>
-            </div>
-            <p class="text-gray-700 mb-6">Experience premium sound quality and industry-leading noise cancellation
-              with
-              these wireless headphones. Perfect for music lovers and frequent travelers.</p>
-
-            <div class="mb-6">
-              <h3 class="text-lg font-semibold mb-2">Color:</h3>
-              <div class="flex space-x-2">
-                <button
-                  class="w-8 h-8 bg-black rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"></button>
-                <button
-                  class="w-8 h-8 bg-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300"></button>
-                <button
-                  class="w-8 h-8 bg-blue-500 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"></button>
-              </div>
-            </div>
-
-            <div class="mb-6">
-              <label for="quantity" class="block text-sm font-medium text-gray-700 mb-1">Quantity:</label>
-              <input type="number" id="quantity" name="quantity" min="1" value="1"
-                class="w-12 text-center rounded-md border-gray-300  shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
-              </input>
-            </div>
-
-            <div class="flex space-x-4 mb-6">
-              <button
-                class="bg-indigo-600 flex gap-2 items-center text-white px-6 py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                  stroke-width="1.5" stroke="currentColor" class="size-6">
-                  <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-                </svg>
-                Add to Cart
-              </button>
-              <button
-                class="bg-gray-200 flex gap-2 items-center  text-gray-800 px-6 py-2 rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                  stroke-width="1.5" stroke="currentColor" class="size-6">
-                  <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                </svg>
-                Wishlist
-              </button>
-            </div>
-
-            <div>
-              <h3 class="text-lg font-semibold mb-2">Key Features:</h3>
-              <ul class="list-disc list-inside text-gray-700">
-                <li>Industry-leading noise cancellation</li>
-                <li>30-hour battery life</li>
-                <li>Touch sensor controls</li>
-                <li>Speak-to-chat technology</li>
-              </ul>
-            </div>
+          <img src={dummyData.propertyImage[0]} alt="Product"
+            class="w-full h-auto rounded-lg shadow-md mb-4" id="mainImage"></img>
+          <div class="flex gap-4 py-4 justify-center overflow-x-auto">
+            <img
+              src={dummyData.propertyImage[1]}
+              alt="Thumbnail 1"
+              className="size-16 sm:size-20 object-cover rounded-md cursor-pointer opacity-60 hover:opacity-100 transition duration-300"
+              onClick={() => changeImage(dummyData.propertyImage[1])}
+            />
+            <img
+              src={dummyData.propertyImage[2]}
+              alt="Thumbnail 2"
+              className="size-16 sm:size-20 object-cover rounded-md cursor-pointer opacity-60 hover:opacity-100 transition duration-300"
+              onClick={() => changeImage(dummyData.propertyImage[2])}
+            />
+            <img
+              src={dummyData.propertyImage[3]}
+              alt="Thumbnail 3"
+              className="size-16 sm:size-20 object-cover rounded-md cursor-pointer opacity-60 hover:opacity-100 transition duration-300"
+              onClick={() => changeImage(dummyData.propertyImage[3])}
+            />
+            <img
+              src={dummyData.propertyImage[4]}
+              alt="Thumbnail 4"
+              className="size-16 sm:size-20 object-cover rounded-md cursor-pointer opacity-60 hover:opacity-100 transition duration-300"
+              onClick={() => changeImage(dummyData.propertyImage[4])}
+            />
           </div>
         </div>
+
+        {/* <!-- Product Details --> */}
+        <div class="w-full md:w-1/2 px-4">
+          <h2 class="text-3xl font-bold mb-2">{dummyData.address}</h2>
+          <p class="text-gray-600 mb-4">{dummyData.city}, {dummyData.province}, {dummyData.postal_code}</p>
+          <div class="mb-4">
+            {/* Ini aku ambil dari yg bid terbesar currently! */}
+            <span class="text-3xl font-bold mr-2 animate-pulse">{dummyData.user[0].bid} ICP</span>
+            <span class="text-gray-500">from {dummyData.startingBid} ICP</span>
+          </div>
+
+          <div class="flex flex-row gap-2">
+            <div class="flex flex-row gap-1.5">
+              <img class="w-6" src="https://img.icons8.com/material-sharp/24/calendar--v1.png" alt="calendar--v1" />
+              <p class="text-md font-medium text-gray-500 ">{dummyData.startBid_date}</p>
+            </div>
+            <div class="flex flex-row gap-1.5">
+              <img class="w-6" src="https://img.icons8.com/ios/50/arrow--v1.png" alt="arrow--v1" />
+              <p class="text-md font-medium text-red-500 ">{dummyData.endBid_date}</p>
+            </div>
+          </div>
+          <p class="text-gray-700 my-6">{dummyData.description}</p>
+
+          {/* Untuk timernya ini, bisa ganti endBid_date dan endBid_hour nya yaa, 
+          Aku BELUM bikin logic semisal countdownnya selesai gimana */}
+
+          <div class="mb-6 flex items-center justify-center md:justify-start w-full gap-4 count-down-main">
+            <div class="timer w-16">
+              <div
+                class=" bg-amber-700 py-4 px-2 rounded-lg overflow-hidden">
+                <h3
+                  class="countdown-element days font-Cormorant font-semibold text-2xl text-white text-center">
+                </h3>
+              </div>
+              <p class="text-lg font-Cormorant font-medium text-gray-900 mt-1 text-center w-full">days</p>
+            </div>
+            <div class="timer w-16">
+              <div
+                class=" bg-amber-700 py-4 px-2 rounded-lg overflow-hidden">
+                <h3
+                  class="countdown-element hours font-Cormorant font-semibold text-2xl text-white text-center">
+                </h3>
+              </div>
+              <p class="text-lg font-Cormorant font-normal text-gray-900 mt-1 text-center w-full">hours</p>
+            </div>
+            <div class="timer w-16">
+              <div
+                class=" bg-amber-700 py-4 px-2 rounded-lg overflow-hidden">
+                <h3
+                  class="countdown-element minutes font-Cormorant font-semibold text-2xl text-white text-center">
+                </h3>
+              </div>
+              <p class="text-lg font-Cormorant font-normal text-gray-900 mt-1 text-center w-full">minutes</p>
+            </div>
+            <div class="timer w-16">
+              <div
+                class=" bg-amber-700 py-4 px-2 rounded-lg overflow-hidden ">
+                <h3
+                  class="countdown-element seconds font-Cormorant font-semibold text-2xl text-white text-center animate-countinsecond">
+                </h3>
+              </div>
+              <p class="text-lg font-Cormorant font-normal text-gray-900 mt-1 text-center w-full">seconds</p>
+            </div>
+          </div>
+
+
+          <div class="flex space-x-4 mb-6">
+            <button
+              class="bg-amber-900 flex gap-2 items-center text-white px-6 py-4 rounded-xl hover:bg-amber-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+              <img class="w-8" src="\images\bidding.png" alt="arrow--v1" />
+              <p class="text-xl">Bid Now!</p>
+            </button>
+
+          </div>
+
+
+        </div>
       </div>
-
-
     </div>
   )
 }
